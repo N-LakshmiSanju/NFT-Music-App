536133a1105e8e0269807769d0986f22e4b32b1d
N-LakshmiSanju <113003954+N-LakshmiSanju@users.noreply.github.com>
2024-02-27 14:09:01 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/Db5AewL5lLDk5QHmCUGFzjrMM66vUbWC",
+      accounts: [ "2b335c196c798754cf6c6da5686078796e3aa5d5360f668af2d32476967c4479" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
